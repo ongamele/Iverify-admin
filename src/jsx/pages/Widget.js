32c6df97c6fa0d651bbf4e339921bdb17b322c08
ogeb@live.com <ogeb@live.com>
2023-09-21 06:20:50 +0200

    update

--- src/jsx/pages/Widget.js
+++ src/jsx/pages/Widget.js
@@ -4,38 +4,7 @@ import { Dropdown, Nav, Tab } from "react-bootstrap";
 /// Scroll
 import PerfectScrollbar from "react-perfect-scrollbar";
 import { Link } from "react-router-dom";
-// images
-import avatar1 from "../../images/avatar/1.jpg";
-import avatar2 from "../../images/avatar/2.jpg";
-import avatar3 from "../../images/avatar/3.jpg";
-import avatar4 from "../../images/avatar/4.jpg";
-import bg1 from "../../images/big/img1.jpg";
-import bg5 from "../../images/big/img5.jpg";
-import profile from "../../images/profile/profile.png";
-import ActiveUser from "./WidgetBasic/ActiveUser";
-import AllSell1 from "./WidgetBasic/AllSell1";
-import AllSell2 from "./WidgetBasic/AllSell2";
-import BloodPressur from "./WidgetBasic/BloodPressure";
-import Clolesterol from "./WidgetBasic/Clolesterol";
-import FeeCollection from "./WidgetBasic/FeeCollection";
-import GlucoseRate from "./WidgetBasic/GlucoseRate";
-import HeartRate from "./WidgetBasic/HeartRate";
-import LifeTimeEarning from "./WidgetBasic/LifeTimeEarning";
-import MarketNow from "./WidgetBasic/MarketNow";
-import NewStudent from "./WidgetBasic/NewStudent";
-import PowerBar from "./WidgetBasic/PowerBar";
-import PowerLine from "./WidgetBasic/PowerLine";
-import SalesAnalysis from "./WidgetBasic/SalesAnalysis";
-import TopProducts1 from "./WidgetBasic/TopProducts1";
-import TopProducts2 from "./WidgetBasic/TopProducts2";
-import TotalCourse from "./WidgetBasic/TotalCourse";
-import TotalStudent from "./WidgetBasic/TotalStudent";
-import ViewProject from "./WidgetBasic/ViewProject";
-import VisitorActivity from "./WidgetBasic/VisitorActivity";
-import WeeklySales1 from "./WidgetBasic/WeeklySales1";
-import WeeklySales2 from "./WidgetBasic/WeeklySales2";
-import Widget1 from "./WidgetBasic/Widget1";
-import Widget2 from "./WidgetBasic/Widget2";
+
 // Page titie
 import PageTitle from "../layouts/PageTitle";
 
@@ -55,7 +24,7 @@ const Widget = () => {
                 <table className="table verticle-middle table-responsive-md">
                   <thead>
                     <tr>
-                      <th scope="col">Ward No.</th>
+                      <th scope="col">Municipality</th>
                       <th scope="col">Name</th>
                       <th scope="col">Surnamee</th>
                       <th scope="col">Date</th>
